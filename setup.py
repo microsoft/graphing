b668f05f802e1f11702e0264631df0f07bf526e3
Rohit Pandey <rohitpandey576@gmail.com>
2022-07-04 13:05:52 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='graphing',
+      version='0.0.8',
+      url='https://github.com/microsoft/graphing',
+      license='MIT',
+      author='Rohit Pandey',
+      author_email='rohitpandey576@gmail.com', 'ropandey@microsoft.com'
+      description='Add static script_dir() method to Path',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      zip_safe=False)
